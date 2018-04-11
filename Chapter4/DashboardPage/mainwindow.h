95b5130be2fe87ed8120e3137e14f069bf86ad20
Virion <leezhieng@gmail.com>
2018-04-11 17:42:20 +0800

    Add files via upload

---
+++ Chapter4/DashboardPage/mainwindow.h
@@ -0,0 +1,51 @@
+#ifndef MAINWINDOW_H
+#define MAINWINDOW_H
+
+#include <QMainWindow>
+
+#include <QtSql>
+#include <QSqlDatabase>
+#include <QSqlQuery>
+
+#include <QDebug>
+#include <QMessageBox>
+
+#include <QtCharts>
+#include <QChartView>
+
+#include <QBarSet>
+#include <QBarSeries>
+
+#include <QPieSeries>
+#include <QPieSlice>
+
+#include <QLineSeries>
+
+namespace Ui {
+class MainWindow;
+}
+
+class MainWindow : public QMainWindow
+{
+	Q_OBJECT
+
+public:
+	explicit MainWindow(QWidget *parent = 0);
+	~MainWindow();
+
+private slots:
+	void on_loginButton_clicked();
+	void on_stackedWidget_currentChanged(int arg1);
+
+	void resizeEvent(QResizeEvent* event);
+
+private:
+	Ui::MainWindow *ui;
+	QSqlDatabase db;
+
+	QChartView *chartViewBar;
+	QChartView *chartViewPie;
+	QChartView *chartViewLine;
+};
+
+#endif // MAINWINDOW_H
