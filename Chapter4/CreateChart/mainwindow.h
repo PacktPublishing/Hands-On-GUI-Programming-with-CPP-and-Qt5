2d6205efbfefdba63f7b11aa74b75821863d04e3
Virion <leezhieng@gmail.com>
2018-04-11 17:40:50 +0800

    Add files via upload

---
+++ Chapter4/CreateChart/mainwindow.h
@@ -0,0 +1,31 @@
+#ifndef MAINWINDOW_H
+#define MAINWINDOW_H
+
+#include <QMainWindow>
+
+#include <QtCharts>
+#include <QChartView>
+
+#include <QBarSet>
+#include <QBarSeries>
+
+#include <QPieSeries>
+#include <QPieSlice>
+
+namespace Ui {
+class MainWindow;
+}
+
+class MainWindow : public QMainWindow
+{
+	Q_OBJECT
+
+public:
+	explicit MainWindow(QWidget *parent = 0);
+	~MainWindow();
+
+private:
+	Ui::MainWindow *ui;
+};
+
+#endif // MAINWINDOW_H
