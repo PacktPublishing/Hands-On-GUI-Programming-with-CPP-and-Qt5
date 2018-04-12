3a070da8d98dda871462192a38068d3e5f23be73
Virion <leezhieng@gmail.com>
2018-04-12 14:56:28 +0800

    Add files via upload

---
+++ Chapter6/Cpp_Js/mainwindow.cpp
@@ -0,0 +1,38 @@
+#include "mainwindow.h"
+#include "ui_mainwindow.h"
+
+MainWindow::MainWindow(QWidget *parent) :
+	QMainWindow(parent),
+	ui(new Ui::MainWindow)
+{
+	ui->setupUi(this);
+
+	webview = new QWebEngineView(ui->verticalFrame);
+	webview->load(QUrl("qrc:///html/test.html"));
+
+	QWebChannel* channel = new QWebChannel(this);
+	channel->registerObject("mainwindow", this);
+	webview->page()->setWebChannel(channel);
+}
+
+MainWindow::~MainWindow()
+{
+	delete ui;
+}
+
+void MainWindow::paintEvent(QPaintEvent *event)
+{
+   QMainWindow::paintEvent(event);
+
+   webview->resize(ui->verticalFrame->size());
+}
+
+void MainWindow::doSomething()
+{
+	ui->label->setText("This text has been changed by javascript!");
+}
+
+void MainWindow::on_pushButton_clicked()
+{
+	webview->page()->runJavaScript("hello();");
+}
