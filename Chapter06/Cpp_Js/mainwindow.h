a77ddb58ef9f007c12141dc79911ffaac33a35c2
Virion <leezhieng@gmail.com>
2018-04-23 05:55:40 +0800

    Update mainwindow.h

--- Chapter06/Cpp_Js/mainwindow.h
+++ Chapter06/Cpp_Js/mainwindow.h
@@ -11,23 +11,23 @@ class MainWindow;
 
 class MainWindow : public QMainWindow
 {
-	Q_OBJECT
+    Q_OBJECT
 
 public:
-	explicit MainWindow(QWidget *parent = 0);
-	~MainWindow();
+    explicit MainWindow(QWidget *parent = 0);
+    ~MainWindow();
 
-	void paintEvent(QPaintEvent *event);
+    Q_INVOKABLE void doSomething();
 
-	Q_INVOKABLE void doSomething();
+protected:
+    virtual void paintEvent(QPaintEvent *event);
 
 private slots:
-	void on_pushButton_clicked();
+    void on_pushButton_clicked();
 
 private:
-	Ui::MainWindow *ui;
-
-	QWebEngineView* webview;
+    Ui::MainWindow *ui;
+    QWebEngineView* webview;
 };
 
 #endif // MAINWINDOW_H
