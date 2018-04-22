54bb7d04ec60a7352240c0140f6b087392a3c026
Virion <leezhieng@gmail.com>
2018-04-23 05:56:25 +0800

    Update mainwindow.cpp

--- Chapter06/Cpp_Js/mainwindow.cpp
+++ Chapter06/Cpp_Js/mainwindow.cpp
@@ -5,34 +5,34 @@ MainWindow::MainWindow(QWidget *parent) :
 	QMainWindow(parent),
 	ui(new Ui::MainWindow)
 {
-	ui->setupUi(this);
+    ui->setupUi(this);
 
-	webview = new QWebEngineView(ui->verticalFrame);
-	webview->load(QUrl("qrc:///html/test.html"));
+    webview = new QWebEngineView(ui->verticalFrame);
+    webview->load(QUrl("qrc:///html/test.html"));
 
-	QWebChannel* channel = new QWebChannel(this);
-	channel->registerObject("mainwindow", this);
-	webview->page()->setWebChannel(channel);
+    QWebChannel* channel = new QWebChannel(this);
+    channel->registerObject("mainwindow", this);
+    webview->page()->setWebChannel(channel);
 }
 
 MainWindow::~MainWindow()
 {
-	delete ui;
-}
-
-void MainWindow::paintEvent(QPaintEvent *event)
-{
-   QMainWindow::paintEvent(event);
-
-   webview->resize(ui->verticalFrame->size());
+    delete ui;
 }
 
 void MainWindow::doSomething()
 {
-	ui->label->setText("This text has been changed by javascript!");
+    ui->label->setText("This text has been changed by javascript!");
+}
+
+void MainWindow::paintEvent(QPaintEvent *event)
+{
+    QMainWindow::paintEvent(event);
+
+    webview->resize(ui->verticalFrame->size());
 }
 
 void MainWindow::on_pushButton_clicked()
 {
-	webview->page()->runJavaScript("hello();");
+    webview->page()->runJavaScript("hello();");
 }
